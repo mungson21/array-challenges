545d237ddf191d32a0076dc3a16c8b32b3fffa99
m ungs <ungson001@live.com>
2021-10-23 16:12:10 -0700

    Challenge #4 complete

--- actions.js
+++ actions.js
@@ -47,6 +47,33 @@ function highPass(arr, cutoff) {
     }
     return filteredArr;
 }
-var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
-console.log(result); // we expect back [6, 8, 10, 9]
+//var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+//console.log(result); // we expect back [6, 8, 10, 9]
+
+//-----------------------------------------------------------------------
+
+//Better than average
+//Given an array of numbers return a count of how many of the numbers are larger than the average.
+
+var i = 0
+
+function betterThanAverage(arr) {
+    var sum = 0;
+    // calculate the average
+    for ( i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    sum /= arr.length;
+
+    var count = 0
+    // count how many values are greater than the average
+    for ( i = 0; i < arr.length; i++) {
+        if (arr[i] > sum) {
+            count++
+        }
+    }
+    return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
 
